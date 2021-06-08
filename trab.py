c6569ebf56295a9d712c5350801cfc34f439401d
Walkerjony <55608271+Walkerjony@users.noreply.github.com>
2021-06-08 04:20:10 -0300

    Update trab.py

--- trab.py
+++ trab.py
@@ -1,78 +1,132 @@
 from random import randint
+
 lista = ("Pedra", "Papel", "Tesoura")
-computador = randint(0, 2)
 count = 0
 count1 = 0
-draw = 0
-i = 1
-nome = input("Digite seu nome!")
+i = 0
 
-choice = input("Digite Y para melhor de 3 ou X para melhor de 5")
+nome = str(input("Digite seu nome"))
+escolha = str(input("y para melhor de 3 ou x para melhor de 5"))
 
-if choice == 'Y':
-
+if escolha == 'y':
     while i < 3:
-        perguntar = int(input('''Escolha uma opcao para se jogar: 
 
-        [0] Pedra
-        [1] Papel
-        [2] Tesoura
- 
-        Digite sua escolha: '''))
+        jogador = int(input('''Escolha uma opcao para se jogar: 
 
-        print(f'O computador escolheu: {lista[computador]}')
-        print(f'O jogador escolheu: {lista[perguntar]}')
+    [0] Pedra
+    [1] Papel
+    [2] Tesoura
+
+    Digite sua escolha: '''))
+        computador = randint(0, 2)
+
+        print(f'O jogador escolheu ---> {lista[jogador]}')
+        print(f'O computador escolheu -----> {lista[computador]}')
 
         if computador == 0:
-            if perguntar == 0:
-                print("Empate")
-                draw += 1
+            if jogador == 0:
                 i += 1
-            elif perguntar == 1:
-                print(f'{nome} Venceu!')
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 1:
+                i += 1
                 count += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 2:
                 i += 1
-            elif perguntar == 2:
-                print("Computador venceu!")
                 count1 += 1
-                i += 1
+                print(f'Placar: {count} X {count1}')
 
 
         elif computador == 1:
-            if perguntar == 0:
-                print("Computador venceu!")
+            if jogador == 0:
+                i += 1
                 count1 += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 1:
                 i += 1
-            elif perguntar == 1:
-                print("Empate!")
-                draw += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 2:
                 i += 1
-            elif perguntar == 2:
-                print(f'{nome} Venceu!')
                 count += 1
+                print(f'Placar: {count} X {count1}')
+
+
+        elif computador == 2:
+            if jogador == 0:
                 i += 1
+                count += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 1:
+                i += 1
+                count += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 2:
+                i += 1
+                print(f'Placar: {count} X {count1}')
+
+if count > count1:
+    print(f'{nome} venceu a partida!')
+if count1 > count:
+    print("Computador venceu a partida!")
+
+if escolha == 'x':
+
+    while i < 5:
+
+        jogador = int(input('''Escolha uma opcao para se jogar: 
+
+    [0] Pedra
+    [1] Papel
+    [2] Tesoura
+
+    Digite sua escolha: '''))
+        computador = randint(0, 2)
+
+        print(f'O jogador escolheu ---> {lista[jogador]}')
+        print(f'O computador escolheu -----> {lista[computador]}')
+
+        if computador == 0:
+            if jogador == 0:
+                i += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 1:
+                i += 1
+                count += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 2:
+                i += 1
+                count1 += 1
+                print(f'Placar: {count} X {count1}')
 
 
-            elif computador == 2:
-                if perguntar == 0:
-                    print(f'{nome} Venceu!')
-                    count += 1
-                    i += 1
-                elif perguntar == 1:
-                    print("Computador venceu!")
-                    count1 += 1
-                    i += 1
-                elif perguntar == 2:
-                    print("Empate!")
-                    draw += 1
-                    i += 1
-
-            if count > count1:
-                print(f'{nome} Venceu a partida!')
-            else:
-                print("Computador venceu a partida!")
+        elif computador == 1:
+            if jogador == 0:
+                i += 1
+                count1 += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 1:
+                i += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 2:
+                i += 1
+                count += 1
+                print(f'Placar: {count} X {count1}')
 
 
+        elif computador == 2:
+            if jogador == 0:
+                i += 1
+                count += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 1:
+                i += 1
+                count += 1
+                print(f'Placar: {count} X {count1}')
+            elif jogador == 2:
+                i += 1
+                print(f'Placar: {count} X {count1}')
 
-                
-
+if count > count1:
+    print(f'{nome} venceu a partida!')
+if count1 > count:
+    print("Computador venceu a partida!")
